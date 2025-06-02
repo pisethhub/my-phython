ab4e2db47f83d7db0d99be49d2fd908a098f2a2e
pisethhub <piseth@api-school.com>
2025-06-02 08:56:16 +0700

    Create Firstproject.py

---
+++ Firstproject.py
@@ -0,0 +1,17 @@
+#Chatbot
+import random
+import time
+chatPlaces = ["the beach", "anywhere with snow", "any big city", "walking in the hills", "my mom house"]
+print("Welcome to Chatbot")
+name = input("What is your name? ")
+print("Hi there", name, "it's great to meet you!")
+place = input("Where is your favourite place to be? ")
+time.sleep(2)
+print("That sounds great, I really love", random.choice(chatPlaces))
+day = input("Have you had a good day? ")
+day = day.lower()
+if day == "yes":
+    print("Good to hear that, me too")
+else:
+    print("Well I hope tomorrow is better")
+print("Really enjoyed the chat, speak to you again soon. ")
